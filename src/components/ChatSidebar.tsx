a864e9b76c537d0bcd7f5d11c9b368b9fe1db1d3
nabbiil._ <nabilsyakir95@gmail.com>
2025-02-24 19:06:13 +0700

    add routing and setup 'react-router'

--- src/components/ChatSidebar.tsx
+++ src/components/ChatSidebar.tsx
@@ -87,13 +87,13 @@ export const ChatSidebar = () => {
         {/* Klik Button untuk membuka dialog */}
         <Button onClick={() => setDialogIsOpen(true)} className="w-full justify-start" variant="ghost">
           <Plus className="mr-2 h-4 w-4" />
-          Chat Baru
+          Percakapan Baru
         </Button>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
-            <SidebarGroupLabel>Recent Chats</SidebarGroupLabel>
+            <SidebarGroupLabel>Percakapan Terbaru</SidebarGroupLabel>
             <SidebarMenu>
               {/* kumpulan thread di sidebar */}
               {/* threads? menandakan awalan kosong */}
@@ -119,7 +119,7 @@ export const ChatSidebar = () => {
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />{" "}
-          Toggle Theme
+          Beralih Tema
         </Button>
       </SidebarFooter>
      </SidebarPrimitive>
