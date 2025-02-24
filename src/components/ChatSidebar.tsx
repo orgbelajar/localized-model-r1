3ab31873c9046f6a16f8e81bc40bf38100c33167
nabbiil._ <nabilsyakir95@gmail.com>
2025-02-24 21:32:14 +0700

    add highlight active thread'

--- src/components/ChatSidebar.tsx
+++ src/components/ChatSidebar.tsx
@@ -1,5 +1,5 @@
 import { Moon, Plus, Sun } from "lucide-react";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import {
   SidebarContent,
@@ -14,11 +14,18 @@ import {
   Sidebar as SidebarPrimitive,
 } from "~/components/ui/sidebar";
 import { useTheme } from "./ThemeProvider";
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { db } from "~/lib/dexie";
-import { useLiveQuery } from 'dexie-react-hooks';
+import { useLiveQuery } from "dexie-react-hooks";
+import { Link, useLocation } from "react-router";
 
 //dummy data
 // const chatGroups = [
@@ -30,11 +37,13 @@ import { useLiveQuery } from 'dexie-react-hooks';
 // ];
 
 export const ChatSidebar = () => {
-  const [activeChat, setActiveChat] = useState<string | null>(null);
+  const [activeThread, setActiveThread] = useState("");
   const [dialogIsOpen, setDialogIsOpen] = useState(false); //toggle dialog kebuka || ketutup
   const [textInput, setTextInput] = useState("");
 
-  const { setTheme, theme } = useTheme();
+  const { setTheme, theme } = useTheme(); 
+
+  const location = useLocation();
 
   const threads = useLiveQuery(() => db.getAllThreads(), []); //trigger seperti useEffect
 
@@ -53,76 +62,86 @@ export const ChatSidebar = () => {
     setTextInput(""); //reset input
   };
 
+  useLayoutEffect(() => {
+    const activeThreadId = location.pathname.split("/")[2]; //id thread
+    setActiveThread(activeThreadId);
+  }, [location.pathname]);
+
   return (
     // react fragment
-   <>
-   {/* following state true || false */}
-   <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}> 
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Buat thread baru</DialogTitle>
-      </DialogHeader>
-  
-        <div className='space-y-1'>
-          <Label htmlFor="thread-title">Judul thread</Label>
-          {/* React Input Handling */}
-          <Input id="thread-title" value={textInput} onChange={(e) => {
-            setTextInput(e.target.value);
-          }}
-          placeholder="Judul thread baru anda"
-          />
-        </div>
- 
-      <DialogFooter>
-        {/* Tutup Dialog */}
-        <Button variant="secondary" onClick={() => setDialogIsOpen(false)}>
-          Batal
-        </Button>
-        <Button onClick={handleCreateThread}>Buat Thread</Button>
-      </DialogFooter>
-    </DialogContent>
-   </Dialog>
-     <SidebarPrimitive>
-      <SidebarHeader>
-        {/* Klik Button untuk membuka dialog */}
-        <Button onClick={() => setDialogIsOpen(true)} className="w-full justify-start" variant="ghost">
-          <Plus className="mr-2 h-4 w-4" />
-          Percakapan Baru
-        </Button>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarGroupLabel>Percakapan Terbaru</SidebarGroupLabel>
-            <SidebarMenu>
-              {/* kumpulan thread di sidebar */}
-              {/* threads? menandakan awalan kosong */}
-              {threads?.map((thread) => (
-                <SidebarMenuItem key={thread.id}>
-                  <SidebarMenuButton
-                    onClick={() => setActiveChat(thread.id)}
-                    isActive={activeChat === thread.id}
-                  >
-                    {thread.title}
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <Button
-          onClick={handleToggleTheme}
-          variant="ghost"
-          className="w-full justify-start"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />{" "}
-          Beralih Tema
-        </Button>
-      </SidebarFooter>
-     </SidebarPrimitive>
-   </>
+    <>
+      {/* following state true || false */}
+      <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Buat thread baru</DialogTitle>
+          </DialogHeader>
+
+          <div className="space-y-1">
+            <Label htmlFor="thread-title">Judul thread</Label>
+            {/* React Input Handling */}
+            <Input
+              id="thread-title"
+              value={textInput}
+              onChange={(e) => {
+                setTextInput(e.target.value);
+              }}
+              placeholder="Judul thread baru anda"
+            />
+          </div>
+
+          <DialogFooter>
+            {/* Tutup Dialog */}
+            <Button variant="secondary" onClick={() => setDialogIsOpen(false)}>
+              Batal
+            </Button>
+            <Button onClick={handleCreateThread}>Buat Thread</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <SidebarPrimitive>
+        <SidebarHeader>
+          {/* Klik Button untuk membuka dialog */}
+          <Button
+            onClick={() => setDialogIsOpen(true)}
+            className="w-full justify-start"
+            variant="ghost"
+          >
+            <Plus className="mr-2 h-4 w-4" />
+            Percakapan Baru
+          </Button>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarGroupLabel>Percakapan Terbaru</SidebarGroupLabel>
+              <SidebarMenu>
+                {/* kumpulan thread di sidebar */}
+                {/* threads? menandakan awalan kosong */}
+                {threads?.map((thread) => (
+                  <SidebarMenuItem key={thread.id}>
+                    {/* dynamic routing berdasarkan id */}
+                    <Link to={`thread/${thread.id}`}> 
+                      <SidebarMenuButton isActive={thread.id === activeThread}>{thread.title}</SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <Button
+            onClick={handleToggleTheme}
+            variant="ghost"
+            className="w-full justify-start"
+          >
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />{" "}
+            Beralih Tema
+          </Button>
+        </SidebarFooter>
+      </SidebarPrimitive>
+    </>
   );
 };
